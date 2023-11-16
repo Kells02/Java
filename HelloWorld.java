95cee039bfefaab7ed4a3bf96cff8520bc905c49
Kells02 <cristianfermar01@gmail.com>
2023-11-16 22:49:36 +0100

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Definimos la clase HelloWorld
+public class HelloWorld {
+    
+    // El método principal que se ejecuta al iniciar el programa
+    public static void main(String[] args) {
+        // Imprimimos "Hello, World!" en la consola
+        System.out.println("Hello, World!");
+    }
+}
